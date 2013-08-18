dbb59400068923caa76031407a2dfe1e27e4423c
Ashley Basinger <ashley.hipps@gmail.com>
2013-08-18 16:18:17 -0400

    finished second page, need rails folders organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SlackerNews::Application.routes.draw do
   get "home/index"
+  get "home/commentspage"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
